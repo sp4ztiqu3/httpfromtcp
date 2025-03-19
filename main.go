7e6b9bf849d913f5e5c6d852565a4a969e18170f
Matt Hiddle <matt.hiddle@gmail.com>
2025-03-19 11:51:46 +0800

    refactor file reading into a function

--- main.go
+++ main.go
@@ -7,32 +7,51 @@ import (
 	"strings"
 )
 
+func getLinesChannel(file io.ReadCloser) <-chan string {
+	ch := make(chan string)
+
+	go func(ch chan string, file io.ReadCloser) {
+		curLine := ""
+		for {
+			data := make([]byte, 8)
+			_, err := file.Read(data)
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				fmt.Println("Error reading contents of file ", err)
+				return
+			}
+			splits := strings.Split(string(data[:]), "\n")
+			curLine += splits[0]
+			if len(splits) > 1 {
+				ch <- curLine
+				curLine = ""
+				curLine += splits[1]
+			}
+		}
+		if curLine != "" {
+			ch <- curLine
+		}
+
+		err := file.Close()
+		if err != nil {
+			fmt.Println("Error closing file - ", err)
+		}
+		close(ch)
+	}(ch, file)
+
+	return ch
+}
+
 func main() {
 	file, err := os.Open("messages.txt")
 	if err != nil {
 		fmt.Println("Unable to open file - ", err)
 		return
 	}
-	curLine := ""
-	for {
-		data := make([]byte, 8)
-		_, err := file.Read(data)
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			fmt.Println("Error reading contents of file ", err)
-			return
-		}
-		splits := strings.Split(string(data[:]), "\n")
-		curLine += splits[0]
-		if len(splits) > 1 {
-			fmt.Printf("read: %s\n", curLine)
-			curLine = ""
-			curLine += splits[1]
-		}
-	}
-	if len(curLine) > 0 {
-		fmt.Printf("read: %s", curLine)
+	ch := getLinesChannel(file)
+	for line := range ch {
+		fmt.Printf("read: %s\n", line)
 	}
 }
